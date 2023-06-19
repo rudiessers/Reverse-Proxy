169cc618640669ab5d931abf42dac57c06ea9b84
esru <rudi.essers@pggm.nl>
2023-06-19 13:33:41 +0200

    Auth 2 ending

--- backend/Microservices.ReverseProxy/Auth/Auth2_Authorisation.cs
+++ backend/Microservices.ReverseProxy/Auth/Auth2_Authorisation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Microservices.ReverseProxy.Auth;
 
@@ -13,33 +14,46 @@ public static class Auth2Authorisation
         builder.Services.AddAuthentication(AuthScheme)
             .AddCookie(AuthScheme)
             .AddCookie(AuthScheme2);
+        
+        builder.Services.AddAuthorization(authorizationOptions =>
+        {
+            authorizationOptions.AddPolicy("eu passport", pb =>
+            {
+                pb.RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(AuthScheme)
+                    .AddRequirements()
+                    .RequireClaim("passport_type", "eur");
+            });
+        });
     }
         
     public static void AddAuth2(this WebApplication app)
     {
         app.UseAuthentication();
-
-        app.Use((ctx, next) =>
-        {
-            if (ctx.Request.Path.StartsWithSegments("/login"))
-            {
-                return next();
-            }
-            
-            if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-            {
-                ctx.Response.StatusCode = 401;
-                return Task.CompletedTask;
-            }
-            if (!ctx.User.HasClaim("passport_type", "eur"))
-            {
-                ctx.Response.StatusCode = 403;
-                return Task.CompletedTask;
-            }
-            
-            return next();
-        });
+        app.UseAuthorization();
         
+        // app.Use((ctx, next) =>
+        // {
+        //     if (ctx.Request.Path.StartsWithSegments("/login"))
+        //     {
+        //         return next();
+        //     }
+        //     
+        //     if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+        //     {
+        //         ctx.Response.StatusCode = 401;
+        //         return Task.CompletedTask;
+        //     }
+        //     if (!ctx.User.HasClaim("passport_type", "eur"))
+        //     {
+        //         ctx.Response.StatusCode = 403;
+        //         return Task.CompletedTask;
+        //     }
+        //     
+        //     return next();
+        // });
+        
+        //[Authorize(Policy = "eu passport")]
         app.MapGet("/unsecure", (HttpContext ctx) =>
         {
             return ctx.User.FindFirst("usr")?.Value ?? "empty";
@@ -59,7 +73,7 @@ public static class Auth2Authorisation
             // }
 
             return "allowed";
-        });
+        }).RequireAuthorization("eu passport");
         
         app.MapGet("/norway", (HttpContext ctx) =>
         {
@@ -97,11 +111,33 @@ public static class Auth2Authorisation
         
         app.MapGet("/login", async (HttpContext ctx) =>
         {
-            var claims = new List<Claim> { new("usr", "anton")};
+            var claims = new List<Claim>();
+            claims.Add(new("usr", "anton") );
             claims.Add(new("passport_type", "eur") );
             var identity = new ClaimsIdentity(claims, AuthScheme);
             var user = new ClaimsPrincipal(identity);
             await ctx.SignInAsync(AuthScheme, user);
-        });
+        }).AllowAnonymous();
+    }
+}
+
+public class MyRequirement : IAuthorizationRequirement
+{
+    
+}
+
+public class MyRequirementHandler : AuthorizationHandler<MyRequirement>
+{
+    public MyRequirementHandler()
+    {
+            
+    }
+    
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MyRequirement requirement)
+    {
+       // context.User
+       // context.Succeed(new MyRequirement());
+
+       return Task.CompletedTask;
     }
 }
